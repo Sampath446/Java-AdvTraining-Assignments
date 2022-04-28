4e7ff2b388708e806cac87ba9eacb8044f79fb94
Sampath446 <sampathkkumar4@gmail.com>
2022-04-28 22:19:55 +0530

    intial commit

---
+++ Core Java Assignments/Problem Statement 7/Problem Statement 7.2/src/com/streambase/NotValidException.java
@@ -0,0 +1,10 @@
+package com.streambase;
+
+public class NotValidException extends Exception{
+	
+	public String validname()
+    {
+         return ("Name is not Valid..Please ReEnter the Name");
+    }
+
+}
